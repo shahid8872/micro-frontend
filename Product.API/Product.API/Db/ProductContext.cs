085a90c3377373acded8cd280cf5217208f9d0bc
shahazan shahid <shahazan.shahid@gmail.com>
2025-04-20 00:39:12 +0800

    Add Customers.API and Product.API to existing project structure

---
+++ Product.API/Product.API/Db/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.API.Model;
+
+namespace Product.API.Db
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+        : base(options)
+        {
+
+        }
+        public DbSet<Product.API.Model.Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
